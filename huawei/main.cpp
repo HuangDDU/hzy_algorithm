575adfb80a30ca7137a4e59c53f65c64588ed883
huang_windows_old <huang@554598474.com>
2023-12-05 21:11:22 +0800

    huawei

---
+++ huawei/main.cpp
@@ -0,0 +1,48 @@
+// 基本模板
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+using namespace std;
+
+// 5 3 2 10
+// 2 3
+// 20 15 10 10 10
+// 10 10 10 20 10
+// 10 15 10 90 10
+// 10 20 10 10 10
+// 10 10 10 10 10
+// 0 0 0
+// 3 1 0
+// 1 3 0
+// 0 10
+// 20 0
+
+int main() {
+  int N, M, F, P;
+  int I, J;
+  vector<vector<int>> G_matrix(N, vector<int>(N)); // 地图矩阵，每个单元的传输代价
+  vector<vector<int>> C_vector(M, vector<int>(3)); // Consumer三元组
+  vector<vector<int>> F_matrix(F, vector<int>(F)); // 转换矩阵
+
+  cin >> N >> M >> F >> P;
+  cin >> I >> J;
+  for (int i = 0; i < N; i++) {
+    for (int j = 0; j < N; j++) {
+      cin >> G_matrix[i][j];
+    }
+  }
+  for (int i = 0; i < M; i++) {
+    cin >> C_vector[i][0] >> C_vector[i][1] >> C_vector[i][2];
+  }
+  for (int i = 0; i < F; i++) {
+    for (int j = 0; j < F; j++) {
+      cin >> F_matrix[i][j];
+    }
+  }
+
+  cout << "" << endl;
+
+  return 0;
+}
