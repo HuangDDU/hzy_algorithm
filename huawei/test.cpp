0495e8692b5604db9498606a570657d43eab67f7
huang_windows_old <huang@554598474.com>
2023-12-16 22:13:59 +0800

    输入修正

--- huawei/test.cpp
+++ huawei/test.cpp
@@ -17,29 +17,29 @@ using namespace std;
 
 int main() {
   int N, M, F, P;
-  int I, J;
+  int I, J;  
+  cin >> N >> M >> F >> P;
   vector<vector<int>> G_matrix(N, vector<int>(N)); // 地图矩阵，每个单元的传输代价
   vector<vector<int>> C_vector(M, vector<int>(3)); // Consumer三元组
   vector<vector<int>> F_matrix(F, vector<int>(F)); // 转换矩阵
-
-  cin >> N >> M >> F >> P;
-  cout << "helloworld1" << endl;
   cin >> I >> J;
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
       cin >> G_matrix[i][j];
     }
   }
+  cout << "G_matrix OK" << endl;
+
   for (int i = 0; i < M; i++) {
     cin >> C_vector[i][0] >> C_vector[i][1] >> C_vector[i][2];
   }
+  cout << "C_vector OK" << endl;
   for (int i = 0; i < F; i++) {
     for (int j = 0; j < F; j++) {
       cin >> F_matrix[i][j];
     }
   }
-
-  cout << "OK" << endl;
+  cout << "F_matrix OK" << endl;
 
   return 0;
 }
